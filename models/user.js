554bf98a7a21ee102ec94aa7d5961c486e11013e
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-22 19:49:13 -0500

    created the imported mongoose and created a schema filled in the required data and used a virtual and exported the user module.

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const {Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+}
+);
+
+// virtual that retrieves the length of the users friends array field on querry.
+UserSchema.virtual('fiendCount').get(function() {
+    return this.friends.length;
+});
+
+
+// create the User model using the schema
+const User = model('User', UserSchema);
+
+module.exports = User;
+
