11d1eab9da4379a88a44b0ab8fe655c9e8568b8c
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-22 20:55:10 -0500

    added the other missing crud methods for updating and deleting.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,7 +37,39 @@ const UserController = {
             console.log(err);
             res.status(400).json(err);
         });
+    },
+
+    // update a user by id
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'})
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // delete a user
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'})
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     }
-}
+};
 
 module.exports = UserController;
