8ac045ec0988d2db18164e2945b65d0fccac434e
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-22 20:23:55 -0500

    built the user controller to use CRUD methods.

---
+++ controllers/user-controller.js
@@ -0,0 +1,43 @@
+const {User} = require('../models/index')
+
+const UserController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        Selection('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    
+    // get user by id
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .then(dbUserData => {
+            // if no user found send 404
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // create a user
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+}
+
+module.exports = UserController;
