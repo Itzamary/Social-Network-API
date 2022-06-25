970643a0828ffb5548144c47fc6316530eada95e
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-25 14:54:20 -0500

    instructed mongoose to populate the thoughts data.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,10 @@ const UserController = {
     // get all users
     getAllUsers(req, res) {
         User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
         .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
