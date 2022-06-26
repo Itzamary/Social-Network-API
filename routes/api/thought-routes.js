92b1b371a3bf90eed2d83ad31597db6c10771fb1
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-25 22:06:31 -0500

    fixed routes to better suite challenge criteria and also added crud methods for added reaction and removed reactions.

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
-const {getAllThoughts, getThoughtById, addThought, updateThought, removeThought} = require('../../controllers/thought-controller');
+const {getAllThoughts, getThoughtById, addThought, addReaction,updateThought, removeThought, removeReaction} = require('../../controllers/thought-controller');
 
 
-// /api/thoughts/<userId>
+// /api/thoughts/
 router
-.route('/:userId')
+.route('/')
 .get(getAllThoughts)
 .post(addThought)
 
-// /api/thoughts/<userId>/<thoughtId>
+// /api/thoughts/<thoughtId>
 router
-.route('/:userId/:thoughtId')
+.route('/:thoughtId')
 .get(getThoughtById)
 .put(updateThought)
 .delete(removeThought)
+
+// /api/thoughts/<thoughtId>/reactions
+router
+.route('/:thoughtId/reactions')
+.put(addReaction)
+
+// /api/thoughts/<thoughtId>/reactions/<reactionsId>
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReaction);
 
 module.exports = router;
