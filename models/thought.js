65c7385c0b23cac59cde5237d02ba6005f8859fb
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-26 18:50:49 -0500

    added max and min length rules to the thought and reaction schemas

--- models/thought.js
+++ models/thought.js
@@ -9,7 +9,7 @@ const ReactionSchema = new Schema(
         reactionBody: {
             type: String,
             required: true,
-
+            maxLength: 280
         },
         username: {
             type: String, 
@@ -33,7 +33,8 @@ const ThoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: true,
-
+            minLength: 1,
+            maxLength: 280
         },
         createdAt: {
             type: Date,
