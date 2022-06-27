5390d4490936f094aa79a6bc9b2f5b8369c8cffd
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-26 19:04:35 -0500

    added a addfriend and remove friend route to the user controller.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,6 +42,26 @@ const UserController = {
             res.status(400).json(err);
         });
     },
+    addFriend({params}, res) {
+        console.log(params)
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {friends: params.friendId}},
+            {new: true}
+        )
+        .then(dbUserData => {
+            console.log(dbUserData, 'addFriendData');
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'})
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err, 'errorAddingFriend');
+            res.status(400).json(err);
+        });
+    },
 
     // update a user by id
     updateUser({params, body}, res) {
@@ -63,6 +83,7 @@ const UserController = {
     deleteUser({params}, res) {
         User.findOneAndDelete({_id: params.id})
         .then(dbUserData => {
+            console.log(dbUserData, 'deleteUserData');
             if (!dbUserData) {
                 res.status(404).json({message: 'No user found with this id'})
                 return;
@@ -73,6 +94,15 @@ const UserController = {
             console.log(err);
             res.status(400).json(err);
         });
+    },
+    removeFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            { $pull: {friends: params.friendId }},
+            {new: true}
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
     }
 };
 
