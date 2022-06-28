9386d87a17fd6dbb521ab69c20299362e9a31a9a
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-27 23:44:18 -0500

    imported the dateformat js file and used it as a getter method in the reacrion and thought schemas.

--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,5 @@
 const {Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema(
     {
@@ -18,6 +19,7 @@ const ReactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: () => dateFormat()
         }
     },
     {
@@ -38,7 +40,8 @@ const ThoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: () => dateFormat()
         },
         username: {
             type: String,
